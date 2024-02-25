4dfee016e685e37b7c4e2523455305063a99d9de
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2024-02-25 11:36:28 +0330

    Delete Gitlab directory

--- Gitlab/Golang/test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-go env -w GOPROXY="https://repo.snapp.tech/repository/goproxy/"
-go env -w GOPATH="$CI_PROJECT_DIR/.cache"
-
-go install "github.com/golang/mock/mockgen@latest"
-
-go test -v ./...
